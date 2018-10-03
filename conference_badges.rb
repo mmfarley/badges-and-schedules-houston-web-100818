47f34b5188f14691b78de3309959794de7aa84f0
Megan Farley <megfarley4@gmail.com>
2018-10-03 19:49:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,11 +18,11 @@ def assign_rooms(names)
   rooms
 end
 
-def printer
-  batch_badge_creator.each do |badge|
+def printer(names)
+  batch_badge_creator(names).each do |badge|
     puts badge
   end
-  assign_rooms.each do |room|
+  assign_rooms(names).each do |room|
     puts room
   end
 end
