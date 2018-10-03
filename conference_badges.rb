810d00fae0cd3eef1e41ac5c612174af849f77c1
Megan Farley <megfarley4@gmail.com>
2018-10-03 19:37:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,17 @@ end
 
 def assign_rooms(names)
   rooms = []
-  names.each_with_index
+  names.each_with_index do |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  rooms
+end
+
+def printer
+  batch_badge_creator.each do |badge|
+    puts badge
+  end
+  assign_rooms.each do |room|
+    puts room
+  end
+end
